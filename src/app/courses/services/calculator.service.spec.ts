912c586ccca881b134130f2e2caf9db7e61e7225
CARAVITA <clement.caravita@sogeti.com>
2023-05-17 17:03:39 +0200

    TestBed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
@@ -7,7 +8,13 @@ describe("CalculatorService", () => {
   beforeEach(() => {
     console.log("Calling beforeEach");
     loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+    calculator = TestBed.inject(CalculatorService);
   });
 
   it("should add two number", () => {
