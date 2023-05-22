6775d0d941fc32f66dc31e14f57416a61287d3f1
CARAVITA <clement.caravita@sogeti.com>
2023-05-22 10:33:07 +0200

    testing data modifications

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -5,6 +5,7 @@ import {
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -40,6 +41,21 @@ describe("CoursesService", () => {
     const req = httpTestingController.expectOne("/api/courses/12");
     expect(req.request.method).toEqual("GET");
     req.flush(COURSES[12]);
+  });
+
+  it("should save the course data", () => {
+    const changes: Partial<Course> = {
+      titles: { description: "Testing Course" },
+    };
+    coursesService.saveCourse(12, changes).subscribe((course) => {
+      expect(course.id).toBe(12);
+    });
+    const req = httpTestingController.expectOne("/api/courses/12");
+    expect(req.request.method).toEqual("PUT");
+    expect(req.request.body.titles.description).toEqual(
+      changes.titles.description
+    );
+    req.flush({ ...COURSES[12], ...changes });
   });
 
   afterEach(() => {
